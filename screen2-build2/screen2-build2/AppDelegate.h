13c7185c5e3ff51d3d5efe62831eb51b1a4e03c3
Siobhan <Siobhan.Schnittger@gmail.com>
2013-10-01 12:25:40 +0100

    App compiliation

---
+++ screen2-build2/screen2-build2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  screen2-build2
+//
+//  Created by Siobhan Schnittger on 27/09/2013.
+//  Copyright (c) 2013 Siobhan Schnittger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ screen2-build2/screen2-build2Tests/screen2_build2Tests.m
@@ -0,0 +1,34 @@
+//
+//  screen2_build2Tests.m
+//  screen2-build2Tests
+//
+//  Created by Siobhan Schnittger on 27/09/2013.
+//  Copyright (c) 2013 Siobhan Schnittger. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface screen2_build2Tests : XCTestCase
+
+@end
+
+@implementation screen2_build2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
